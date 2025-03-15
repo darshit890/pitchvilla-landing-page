fd0656ea0ca9856b3d83b889e0700373cc749b73
darshit890 <88980439+darshit890@users.noreply.github.com>
2025-03-15 19:13:30 +0530

    Update PiitchDeck.tsx

--- src/components/PiitchDeck.tsx
+++ src/components/PiitchDeck.tsx
@@ -18,9 +18,10 @@ const PitchDeck = () => {
       {/* Absolute positioning for perfect centering */}
       <div className="absolute inset-0 flex items-center justify-center">
         <div className="flex flex-col items-center text-center">
-          <h1 className="mb-8 font-bold text-4xl text-purple-900">
-            NOW ACCEPTING PITCH DECK
-          </h1>
+        <h1 className="mb-8 font-bold text-3xl sm:text-4xl md:text-2xl lg:text-5xl text-purple-900">
+    NOW ACCEPTING PITCH DECK
+</h1>
+
           
           <div className="flex flex-col gap-4 w-80">
             <button className="rounded-full bg-gradient-to-r from-[#6500AB] to-[#290045] px-8 py-3 font-bold text-white">
