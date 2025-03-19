fb06cb96058ff1630458a7efc8a8dd5700b9a0b7
darshit890 <88980439+darshit890@users.noreply.github.com>
2025-03-20 00:10:07 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,44 +1,5 @@
-    import ClientNavbar from "./ClientNavbar";
+import ClientNavbar from "./ClientNavbar";
 import Image from "next/image";
-
-
-interface NavItem {
-  name: string;
-  href: string;
-  subItems?: { name: string; href: string }[];
-}
-
-// Define navItems outside both components so it's accessible to both
-export const navItems: NavItem[] = [
-  {
-    name: "STARTUPS",
-    href: "#startups",
-    subItems: [
-      { name: "PITCH DECK", href: "#pitch-deck" },
-      { name: "STARTUP TOOLKIT", href: "#startup-toolkit" },
-      { name: "VALUATION REPORT", href: "#valuation-report" },
-      { name: "BUSINESS REVIEW REPORT", href: "#business-review-report" },
-      { name: "COMPANY COMPLIANCE", href: "#company-compliance" },
-      { name: "FOUNDER COUNCELLING", href: "#founder-councelling" },
-      { name: "VIRTUAL CXO", href: "#virtual-cxo" },
-    ],
-  },
-  {
-    name: "INVESTOR",
-    href: "#investor",
-    subItems: [
-      { name: "STARTUP HUNTER", href: "#startup-hunter" },
-      { name: "STARTUP DUE DILIGENCE", href: "#startup-due-diligence" },
-      { name: "CO-INVESTING OPPORTUNITY", href: "#co-investing" },
-      { name: "INVESTOR ELITE CLUB", href: "#investor-elite-club" },
-      { name: "IPO DEALS & ASSIST", href: "#ipo-deals" },
-      { name: "MERGERS AND ACQUISITIONS", href: "#mergers-acquisitions" },
-    ],
-  },
-  { name: "STUDENT", href: "#student" },
-  { name: "BLOGS", href: "#blogs" },
-  { name: "CONTACT US", href: "#contact" },
-];
 
 const Navbar = () => {
   return (
@@ -50,8 +11,7 @@ const Navbar = () => {
               <Image src={"/image-Photoroom 1 (2).png"} alt="logo" width={150} height={150} />
             </div>
           </div>
-
-          {/* Pass the client-side functionality to ClientNavbar */}
+          
           <ClientNavbar />
         </div>
       </div>
