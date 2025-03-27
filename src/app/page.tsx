7ec09eff1b7cbe19948757e91cdc3f6e95ed24fc
darshit890 <88980439+darshit890@users.noreply.github.com>
2025-03-27 13:38:06 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,17 @@
 import CampusAmbassador from '@/components/CampusAmbassador';
 import Hero from '@/components/Hero';
 import ServicesPage from '@/components/Services';
+import GetApp from '@/components/GetApp';
 // import PitchDeck from '@/components/PitchDeck';
-// import GetApp from '@/components/GetApp';
 
+   
 export default function HeroSection() {  
   return (  
     <>
     <Hero />
     <ServicesPage />
 {/*     <PitchDeck /> */}
-{/*     <GetApp /> */}
+     <GetApp /> 
     <CampusAmbassador />
     </>
   );  
